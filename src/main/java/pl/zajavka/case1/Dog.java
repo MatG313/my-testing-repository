441c2aa6a68ec8c7684545b3c80c61500017e44d
Mateusz Gajewski <mateo31313@gmail.com>
2023-06-04 18:19:47 +0200

    Modification 4

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,13 +1,16 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
+    @Override
+    public String whatIsYourName () {
+        return "My name is Doggo";
+    }
+
     private String name;
 
     public void sniff() {
         System.out.println("I'm soo sniffing!");
 
-    @Override
-    public String whatIsYourName() {
-        return "My name is Doggo";
+
     }
 }
